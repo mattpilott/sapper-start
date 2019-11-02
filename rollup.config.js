323d124010de6520cafa391f91e1a8dc09d33689
Matt Pilott <matt@creativelittledots.co.uk>
2019-11-02 12:30:09 +0000

    Extensive rollup.config.js updates

--- rollup.config.js
+++ rollup.config.js
@@ -1,66 +1,69 @@
+/* Rollup plugins */
+import alias from '@rollup/plugin-alias';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
-import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
-import config from 'sapper/config/rollup.js';
-import pkg from './package.json';
-import sveltePreprocess from 'svelte-preprocess';
-import autoprefixer from 'autoprefixer';
-import alias from 'rollup-plugin-alias';
-import path from 'path';
 
+/* Packages */
+import autoprefixer from 'autoprefixer';
+import config from 'sapper/config/rollup.js';
+import path from 'path';
+import pkg from './package.json';
+import { scss, postcss } from 'svelte-preprocess';
+
+/* Assignments */
+const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
-const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
-const preprocess = sveltePreprocess({
-   transformers: {
-      scss: { sourceMap: false },
-      postcss: { plugins: [ autoprefixer ] }
-   }
-});
+const dedupe = importee => importee === 'svelte' || importee.startsWith('svelte/');
+const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
 
 const aliasconfig = {
-   resolve: ['.js', '.mjs', '.html', '.svelte'],
-   entries:[{
-      find:'~',
-      replacement: path.join(__dirname, './src')
-   }]
+   resolve: ['.js', '.mjs', '.html', '.svelte', '.scss'],
+   entries:[{ find: '~', replacement: path.join(__dirname, './src') }]
 };
 
+/* Config */
 export default {
+
 	client: {
 		input: config.client.input(),
 		output: config.client.output(),
+      onwarn,
 		plugins: [
+         alias(aliasconfig),
+         commonjs(),
 			replace({
 				'process.browser': true,
 				'process.env.NODE_ENV': JSON.stringify(mode),
             'pkg.version': JSON.stringify(pkg.version)
 			}),
+         resolve({ browser: true, dedupe }),
 			svelte({
 				dev,
 				hydratable: true,
 				emitCss: true,
-            preprocess
+            preprocess: [
+               scss({ sourceMap: false }),
+               postcss({ plugins: [ autoprefixer ] })
+            ]
 			}),
-			resolve(),
-			commonjs(),
-         alias(aliasconfig),
 
 			legacy && babel({
+            exclude: ['node_modules/@babel/**'],
 				extensions: ['.js', '.mjs', '.html', '.svelte'],
-				runtimeHelpers: true,
-				exclude: ['node_modules/@babel/**'],
-				presets: [['@babel/preset-env']],
 				plugins: [
 					'@babel/plugin-syntax-dynamic-import',
 					['@babel/plugin-transform-runtime', {
 						useESModules: true
 					}]
-				]
+				],
+            presets: [['@babel/preset-env']],
+            runtimeHelpers: true,
 			}),
 
 			!dev && terser({
@@ -73,20 +76,24 @@ export default {
 	server: {
 		input: config.server.input(),
 		output: config.server.output(),
+      onwarn,
 		plugins: [
+         alias(aliasconfig),
+         commonjs(),
 			replace({
 				'process.browser': false,
 				'process.env.NODE_ENV': JSON.stringify(mode),
             'pkg.version': JSON.stringify(pkg.version)
 			}),
+         resolve({ dedupe }),
 			svelte({
+            dev,
 				generate: 'ssr',
-				dev,
-            preprocess
-			}),
-			resolve(),
-			commonjs(),
-         alias(aliasconfig),
+            preprocess: [
+               scss({ sourceMap: false }),
+               postcss({ plugins: [ autoprefixer ] })
+            ]
+			})
 		],
 		external: Object.keys(pkg.dependencies).concat(
 			require('module').builtinModules || Object.keys(process.binding('natives'))
@@ -96,15 +103,16 @@ export default {
 	serviceworker: {
 		input: config.serviceworker.input(),
 		output: config.serviceworker.output(),
+      onwarn,
 		plugins: [
-			resolve(),
+         alias(aliasconfig),
+         commonjs(),
 			replace({
 				'process.browser': true,
 				'process.env.NODE_ENV': JSON.stringify(mode),
             'pkg.version': JSON.stringify(pkg.version)
 			}),
-			commonjs(),
-         alias(aliasconfig),
+         resolve(),
 			!dev && terser({numWorkers: 1})
 		]
 	}
