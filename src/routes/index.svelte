2d5f46a9509b89157c93cb5d9393be63ab6f3ca3
Matt Pilott <matt@creativelittledots.co.uk>
2020-03-12 20:42:05 +0000

    deps and minor h1 style update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,4 @@
-<h1>&nbsp;&nbsp;&nbsp;Sapper launched successfully! 🚀</h1>
+<h1 style="position:fixed;width:100%;top:40vh;text-align:center">Sapper launched successfully! 🚀</h1>
 
 <script context="module">
 
