0a7980a3bbc90c4d19acb041acf69e520c5da057
Matt Pilott <matt@creativelittledots.co.uk>
2020-03-16 20:12:09 +0000

    added minor SEO benefits, robots.txt and trailing slash redirects

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import { sanslash } from '@neuekit/utilities/middleware';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
+	.use(sanslash)
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
