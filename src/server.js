96c95f1a95c64bed90c7cbb07f13e27aee468d81
Matt Pilott <matt@creativelittledots.co.uk>
2020-07-20 15:55:39 +0100

    deps and dotenv for production

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 import { sanslash } from '@neuekit/utilities/src/middleware';
+import 'dotenv/config';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
